6d5a929a1ca08a156067959ca7b8d01196ca9d92
as-good-study <2992742880@qq.com>
2022-04-21 23:07:26 +0800

    20220421-23:07

--- 20220115/Project1/Project1/main.cpp
+++ 20220115/Project1/Project1/main.cpp
@@ -97,59 +97,80 @@ using namespace std;
 //}
 
 
-class Base
-{
-public:
-	void func(void);
-	int m_A;
-};
+//class Base
+//{
+//public:
+//	void func(void);
+//	int m_A;
+//};
+//
+//void Base::func(void)
+//{
+//	m_A = 100;
+//}
+//
+//class Son
+//{
+//public:
+//	void func(void);
+//	int m_A;
+//};
+//
+//void Son::func(void)
+//{
+//	//this->m_A = Base::m_A; //这样的赋值，右边的写法语法不允许
+//	m_A = 200;
+//}
+//
+//void test(void)
+//{
+//	Son s;
+//	s.func();
+//	//s.m_A = Base::m_A; //这样的赋值，右边的写法语法不允许
+//	cout << "Son m_A: " << s.m_A << endl;
+//}
+//
+//int main(void)
+//{
+//	test();
+//	char arr[] = "hello";
+//	char* arr1 = NULL;
+//	//char* arr2 = "hellow"; //这种写法错误
+//	cout << "arr[] : " << sizeof(arr) << endl;
+//	cout << "arr1 : " << sizeof(arr1) << endl;
+//	cout << "short : " << sizeof(short) << endl;
+//	cout << "长度=: " << strlen("c:\test\628\test.c") << endl; //这个\t是占用一个字节
+//
+//	int i = 0;
+//	int k = 0;
+//	for (i = 0, k = 0; k = 0; i++, k++) //不进入循环，因为循环判断条件就是0
+//	{
+//		k++;
+//		cout << "hehe" << endl;
+//	}
+//		
+//
+//	system("pause");//主要时防止程序调试的时候闪退
+//	return EXIT_SUCCESS;
+//}
 
-void Base::func(void)
-{
-	m_A = 100;
-}
+//#include "easyx.h" //图形界面头文件
+//#include "mmsystem.h" //多媒体系统头文件
+//#pragma comment(lib,"winmm.lib") //C - 预处理库文件包含
+////#include <easyx>
+////学习地址-夜曲.mp3：https://www.bilibili.com/video/BV1ga411B71U?spm_id_from=333.337.search-card.all.click
+//int main(void)
+//{
+//	initgraph(480, 680);
+//
+//	system("pause");
+//}
 
-class Son
-{
-public:
-	void func(void);
-	int m_A;
-};
-
-void Son::func(void)
-{
-	//this->m_A = Base::m_A; //这样的赋值，右边的写法语法不允许
-	m_A = 200;
-}
-
-void test(void)
-{
-	Son s;
-	s.func();
-	//s.m_A = Base::m_A; //这样的赋值，右边的写法语法不允许
-	cout << "Son m_A: " << s.m_A << endl;
-}
-
-int main(void)
-{
-	test();
-	char arr[] = "hello";
-	char* arr1 = NULL;
-	//char* arr2 = "hellow"; //这种写法错误
-	cout << "arr[] : " << sizeof(arr) << endl;
-	cout << "arr1 : " << sizeof(arr1) << endl;
-	cout << "short : " << sizeof(short) << endl;
-	cout << "长度=: " << strlen("c:\test\628\test.c") << endl; //这个\t是占用一个字节
-
-	int i = 0;
-	int k = 0;
-	for (i = 0, k = 0; k = 0; i++, k++) //不进入循环，因为循环判断条件就是0
-	{
-		k++;
-		cout << "hehe" << endl;
-	}
-		
-
-	system("pause");
-	return EXIT_SUCCESS;
-}
+//int main()
+//{
+//    int arr[] = { 11,12,13,14,15 };
+//    int* ptr = arr;
+//    *(ptr++) += 100;
+//    printf("%d %d\n", *ptr, *(++ptr));
+//    return 0;
+//}
